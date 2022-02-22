5935b40ff2cc8238226969c730de3ce9e93e4745
huaxing <wang.huax@northeastern.edu>
2022-02-21 18:56:45 -0800

    add new file

---
+++ projects/React-Counter/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import CC from './CC'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>A little counter</h1>
+    <CC/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ projects/React-Counter/src/CC.js
@@ -0,0 +1,34 @@
+import React, { Component} from 'react';
+import './counter.css'
+
+
+class CC extends React.Component{
+
+  constructor(){
+    super();
+    this.state={
+      number:0,
+    }
+  }
+
+  add=()=>{
+    this.setState({number:this.state.number+1})
+
+  }
+
+  subtract=()=>{
+    this.setState({number:this.state.number-1})
+
+  }
+
+
+  render(){
+    return <div>
+      <h1>{this.state.number}</h1>
+
+      <button onClick={this.add}>plus 1</button>
+      <button onClick={this.subtract}>minus 1</button>
+    </div>
+  }
+}
+export default CC;
